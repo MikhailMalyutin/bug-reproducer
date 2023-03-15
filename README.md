0dde2d72a831ac7770ca369e57f89c480c063270
mmalyutin <Mihail.Malyutin@bia-tech.ru>
2023-03-15 09:33:28 +0300

    Added bug description

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 For reproduce https://github.com/quarkusio/quarkus/discussions/31807
+
+To reproduce run
+./gradlew quarkusDev
+
+In browser open
+http://localhost:8080/q/swagger-ui/#/Bug%20Reproduce%20Service/post_data
+
+See error section
+Resolver error at paths./data.post.requestBody.content.multipart/form-data.schema.properties.fileStream.allOf.0.$ref
+Could not resolve reference: Could not resolve pointer: /components/schemas/File does not exist in document
+Resolver error at paths./data.post.requestBody.content.multipart/form-data.schema.properties.fileStream.$ref
+Could not resolve reference: Could not resolve pointer: /components/schemas/File does not exist in document
+
+If you open
+http://localhost:8080/q/swagger-ui/#/Bug%20Reproduce%20Service/post_upload1 then
+you see uploadButton works. But if implement saving file looks like InputStream is uploaded as text, not binary file
--- src/main/kotlin/test/BugReproduceService.kt
+++ src/main/kotlin/test/BugReproduceService.kt
@@ -1,24 +1,18 @@
 package test
 
-import com.fasterxml.jackson.annotation.JsonProperty
-import org.eclipse.microprofile.openapi.annotations.Operation
 import org.eclipse.microprofile.openapi.annotations.enums.SchemaType
 import org.eclipse.microprofile.openapi.annotations.media.Content
 import org.eclipse.microprofile.openapi.annotations.media.Schema
-import org.eclipse.microprofile.openapi.annotations.parameters.Parameter
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponse
 import org.jboss.resteasy.reactive.PartType
 import org.jboss.resteasy.reactive.RestForm
 import java.io.File
 import java.io.InputStream
 import java.util.*
-import javax.annotation.security.RolesAllowed
 import javax.ws.rs.Consumes
 import javax.ws.rs.POST
 import javax.ws.rs.Path
-import javax.ws.rs.WebApplicationException
 import javax.ws.rs.core.MediaType
-import javax.ws.rs.core.Response
 
 
 class FileForm {
@@ -32,17 +26,39 @@ class FileForm {
     lateinit var fileStream: File
 }
 
+class FileForm1 {
+    @RestForm("fileName")
+    @PartType(MediaType.TEXT_PLAIN)
+    var fileName: String = ""
+
+    @RestForm("file")
+    @PartType(MediaType.APPLICATION_OCTET_STREAM)
+    @field:Schema(type= SchemaType.STRING, description = "Input file", format = "binary")
+    lateinit var fileStream: InputStream
+}
+
 @Path("/")
 class BugReproduceService {
     @POST
     @Consumes(MediaType.MULTIPART_FORM_DATA)
     @Path("data")
-    @Operation(description = "Создание данных", summary = "Создание данных")
     @APIResponse(responseCode = "200", content = [Content(
         mediaType = MediaType.APPLICATION_JSON
     )])
     fun createData(
         file: FileForm
+    ) {
+        TODO()
+    }
+
+    @POST
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    @Path("upload1")
+    @APIResponse(responseCode = "200", content = [Content(
+        mediaType = MediaType.APPLICATION_JSON
+    )])
+    fun createData(
+        file: FileForm1
     ) {
         TODO()
     }
